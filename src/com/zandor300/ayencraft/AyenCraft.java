644e8d232c046d21757eeab7d2163ebb28638910
Zandor300 <info@zsinfo.nl>
2014-12-31 13:07:07 +0100

    Initial commit.

---
+++ src/com/zandor300/ayencraft/AyenCraft.java
@@ -0,0 +1,33 @@
+package com.zandor300.ayencraft;
+
+import com.zandor300.core.utilities.Chat;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Zandor300.
+ * Date: 31-12-2014
+ * Time: 13:03
+ * PC: ZANDOR-PC
+ */
+public class AyenCraft extends JavaPlugin{
+
+	private static Chat chat = new Chat("AyenCraft", ChatColor.DARK_GREEN);
+
+	@Override
+	public void onEnable() {
+		chat.sendConsoleMessage("Setting plugin up...");
+
+
+
+		chat.sendConsoleMessage("AyenCraft enabled.");
+	}
+
+	@Override
+	public void onDisable() {
+		chat.sendConsoleMessage("Cleaning server up...");
+
+		chat.sendConsoleMessage("Everything is snining again!");
+		chat.sendConsoleMessage("AyenCraft disabled.");
+	}
+}
